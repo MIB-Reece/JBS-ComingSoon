1a6ad89827ede3e6d532f76c524a12cdde2aa186
MIB-Reece <mauriceonyeani@gmail.com>
2021-11-30 03:28:09 -0500

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from "yup";
+import * as Yup from 'yup';
 import {
   Grid,
   CssBaseline,
